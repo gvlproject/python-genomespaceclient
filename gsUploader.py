7317e38f5c3e1490a9bde466b31c5b24d7d37f5a
ykowsar <ykowsar@gmail.com>
2016-02-16 11:45:47 +1100

    Re-architect the code to follow Abstract factory pattern

--- gsUploader.py
+++ gsUploader.py
@@ -1,39 +1,3 @@
-import gsLogin
-from abc import ABCMeta, abstractmethod
-import requests.packages.urllib3.util.ssl_
-requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL'
-
-
-class Uploader:
-    __metaclass__ = ABCMeta
-    def requestUpload(self, gsUserName, gsPasword, gsURL, gsDNS):
-        print "Uploading: " + gsURL
-        r = requests.get(gsURL, cookies = gsLogin.getGenomeSpaceToken(gsUserName,gsPasword, gsDNS), headers = { 'Content-type': 'application/json' })
-        return r.text
-
-    def requestUpload(self, gsToken, gsURL, gsDNS):
-        print "Uploading: " + gsURL
-        r = requests.get(gsURL, cookies={"gs-token" :gsToken}, headers = { 'Content-type': 'application/json' })
-        return r.text
-#
-#
-# 	def requestUpload(self, gsUserName, gsPasword, gsURL, gsDNS):
-# 		opener = urllib2.build_opener(urllib2.HTTPHandler)
-# 		request = urllib2.Request(gsURL)
-#
-# 		request.add_header('Cookie',gsLogin.getGenomeSpaceToken(gsUserName,gsPasword, gsDNS))
-#
-# 		request.add_header('Content-Type', 'application/json')
-# 		try:
-# 			resp = opener.open(request)
-# 			return resp.read()
-# 		except urllib2.HTTPError as e:
-# 			print e.read()
-
-	@abstractmethod
-	def upload(self, jsnObject, filePath):
-		'''This method will recieve jsonObject from genomespace response for requesting a file upload. Each uploader should use the json object to talk t the specific storage type'''
-		return
 
 
 class test(Uploader):
---
+++ upload2GenomeSpace.py
@@ -0,0 +1,36 @@
+import argparse
+from uploadFactory import uploadFactory, Uploader
+import re
+
+def upload_file_to_genomespace_bytoken(server, gsToken, target_url, local_filename):
+    typeReg =re.search('(?<=Home.)\w+', a)
+    uploaderType = typeReg.group(0)
+    uploader = uploadFactory.getUploader(uploaderType)
+    upload_request = uploader.requestUpload(gsToken, target_url.replace("/datamanager/v1.0/file/", "/datamanager/v1.0/uploadinfo/"), server)
+    uploader.upload(upload_request, local_filename)
+
+#def upload_file_to_genomespace_byswiftCleint(server, gsToken, target_url, local_filename):
+#    uploaderType = ""
+#    uploader = uploadFactory.getUploader(uploaderType)
+#    upload_request = uploader.requestUpload(gsToken, target_url.replace("/datamanager/v1.0/file/", "/datamanager/v1.0/uploadinfo/"), server)
+#    uploader.upload(upload_request, local_filename)
+
+def upload_file_to_genomespace(server, user, password, target_url, local_filename):
+    uploaderType = ""
+    uploader = uploadFactory.getUploader(uploaderType)
+    upload_request = uploader.requestUpload(user, password, target_url.replace("/datamanager/v1.0/file/", "/datamanager/v1.0/uploadinfo/"), server)
+    uploader.upload(upload_request, local_filename)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--server', type=str, help="Genomespace server name", required=True)
+    parser.add_argument('-u', '--user', type=str, help="Genomespace username", required=False)
+    parser.add_argument('-p', '--password', type=str, help="Genomespace password", required=False)
+    parser.add_argument('-t', '--target_url', help="Genomespace target URI of file to upload", required=True)
+    parser.add_argument('-f', '--file', type=str, help="Local file to upload", required=True)
+    parser.add_argument('-n', '--token', type=str, help="GenomeSpace valid token to talk to GenomeSpace", required=False)
+    args = parser.parse_args()
+    if len(args.token)>0:
+        upload_file_to_genomespace_bytoken(args.server, args.token, args.target_url, args.file)
+    else:
+        upload_file_to_genomespace(args.server, args.user, args.password, args.target_url, args.file)#a = test()
