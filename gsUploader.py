4bdbd722ebf0c5fa8cd9fd496765452ab6c466f1
Nuwan Goonasekera <nuwan.ag@gmail.com>
2016-02-20 21:56:01 +1100

    Removed unneeded files

--- gsUploader.py
+++
@@ -1,9 +0,0 @@
-
-
-class test(Uploader):
-
-	def upload(self, jsnObject, filePath):
-		return
-
-#a = test()
-#print a.requestUpload("devtest","devtest", "https://genomespace-dev.genome.edu.au/datamanager/v1.0/uploadinfo/Home/swift:Demo2/test1.png", "genomespace.genome.edu.au")
