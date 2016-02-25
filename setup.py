1e3d457a09a4c17fcfe0367a7ee151f590e429ab
Nuwan Goonasekera <nuwan.ag@gmail.com>
2016-02-26 01:58:54 +1100

    Bumped patch version for pip release.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='python-genomespaceclient',
-      version="0.1.3",
+      version="0.1.4",
       description='Python bindings and commandline client to the GenomeSpace'
       ' API',
       author='GVL Project',
